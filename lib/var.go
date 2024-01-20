310375a02d9d97ce38909cebad9c5372bee6890a
Placeless <mikoprpr@hotmail.com>
2024-01-21 08:41:06 +0900

    move code

--- lib/var.go
+++ lib/var.go
@@ -4,24 +4,3 @@ import (
 	"fmt"
 )
 
-// 変数宣言
-func Var() {
-	// 整数型の変数
-	var num1 int = 123
-
-	// 右辺で型が決まるなら型名は省略可能
-	var num2 = 456
-
-	// 省略記法（関数内でのみ利用可能）
-	num3 := 789
-
-	fmt.Println(num1)
-	fmt.Println(num2)
-	fmt.Println(num3)
-}
-/*
-出力：
-123
-456
-789
-*/
