a1f20633df670d790cbe035cb489c34584df22d3
Placeless <mikoprpr@hotmail.com>
2024-01-22 00:50:46 +0900

    gpt

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
+	"パッケージのパス/ard_servo2" // この行を修正する
+)
+
 func main() {
-	ard_servo2()
+	firmataAdaptor := firmata.NewAdaptor("/dev/ttyACM0") // Arduinoのポートに適切に変更してください
+	servo := ard_servo2.NewServoDriver(firmataAdaptor, "9") // この行を修正する
+
+	work := func() {
+		fmt.Println("Starting servo...")
+
+		gobot.Every(1*time.Second, func() {
+			// 0度から180度までの範囲でサーボモーターを回転
+			for angle := 0; angle <= 180; angle += 30 {
+				fmt.Printf("Moving servo to %d degrees\n", angle)
+				servo.Move(uint8(angle))
+				time.Sleep(1 * time.Second)
+			}
+
+			// サーボモーターを初期位置に戻す
+			fmt.Println("Moving servo to initial position")
+			servo.Move(90)
+			time.Sleep(1 * time.Second)
+		})
+	}
+
+	robot := gobot.NewRobot("servoBot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{servo},
+		work,
+	)
+
+	robot.Start()
 }
