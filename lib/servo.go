29fccfb479e868f39c3e3d2965740f9c3716e239
Placeless <mikoprpr@hotmail.com>
2024-01-21 20:25:06 +0900

    install 先書き換えなどした

---
+++ lib/servo.go
@@ -0,0 +1,34 @@
+package lib
+
+import (
+  "fmt"
+  "time"
+
+  "gobot.io/x/gobot" // go get -d -u gobot.io/x/gobot で install
+  "gobot.io/x/gobot/platforms/firmata" // go get gobot.io/x/gobot/platforms/firmata でインストール
+  "github.com/warthog618/gpio" // go get github.com/warthog618/gpio でインストール
+)
+
+func main() {
+  gbot := gobot.NewGobot()
+
+  firmataAdaptor := firmata.NewFirmataAdaptor("firmata", "/dev/tty.usbmodem1411")
+  servo := gpio.NewServoDriver(firmataAdaptor, "servo", "3")
+
+  work := func() {
+    gobot.Every(1*time.Second, func() {
+      i := uint8(gobot.Rand(180))
+      fmt.Println("Turning", i)
+      servo.Move(i)
+    })
+  }
+
+  robot := gobot.NewRobot("servoBot",
+    []gobot.Connection{firmataAdaptor},
+    []gobot.Device{servo},
+    work,
+  )
+
+  gbot.AddRobot(robot)
+  gbot.Start()
+}
