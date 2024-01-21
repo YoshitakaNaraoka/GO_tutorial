8388c284d7f8f274d4ec921486e196d391fbe3d6
Placeless <mikoprpr@hotmail.com>
2024-01-22 00:45:23 +0900

    repair entrypoint

--- main.go
+++ main.go
@@ -1,30 +1,8 @@
 package main
 
 import (
-	"GO_tutorial/lib"
-	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	lib.Lit()
-	lib.Shadow()
-	lib.Var()
-	lib.DataType()
-	lib.Array()
-	lib.Map()
-	lib.IfSeigyo()
-	lib.ForSeigyo()
-	lib.SwitchSeigyo()
-	lib.Func1()
-	lib.Func1_2()
-	lib.Func2("国語","50",20)
-	lib.Func3("学校")
-	lib.Func4()
-	lib.Func5()
-	lib.Func6()
-	lib.Func8()
-	lib.Func10_2()
-	lib.Allfunc()
 	ard_servo()
 }
